f93b021afac81eb16cad2e0bfc60bfe91ecc7418
Michelle Khan <michelle.khan1@outlook.com>
2022-02-02 14:22:12 +0100

    test_get_persons_success OK [deni denisé nick michelle]

--- src/main/java/com/example/personsrest/domain/PersonController.java
+++ src/main/java/com/example/personsrest/domain/PersonController.java
@@ -4,11 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/persons/")
 public class PersonController {
+    List<String> person = new ArrayList<>();
     @GetMapping
-    public PersonTest getPerson(){
-        return new PersonTest("michelle", 25, "malmö", "3");
+    public CreatePerson createPerson() {
+        CreatePerson createPerson = new CreatePerson("Arne Anka", "Ankeborg", 5);
+        person.add(String.valueOf(createPerson));
+        return createPerson;
+    }
+
+    @GetMapping("/testperson")
+    public String getPerson() {
+        return person.get(0);
     }
 }
