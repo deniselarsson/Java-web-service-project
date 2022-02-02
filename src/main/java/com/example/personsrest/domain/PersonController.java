6e7760508a0c23f4ea40d9eb9927e0adb2ef435d
Michelle Khan <michelle.khan1@outlook.com>
2022-02-02 13:42:33 +0100

    added implementation classes [deni denisé nick michelle]

--- src/main/java/com/example/personsrest/domain/PersonController.java
+++ src/main/java/com/example/personsrest/domain/PersonController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("/api/persons/")
 public class PersonController {
     @GetMapping
     public PersonTest getPerson(){
